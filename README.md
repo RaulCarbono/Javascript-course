e874153a67acaa59d124adb9ac516a7cb51b95b5
Raul Carbono <carbonoraul2@gmail.com>
2022-06-07 23:21:26 -0500

    jsPruebas

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Javascript-course
+
+...
+
+## Taller #1 : Figuras geometricas
+
+- Primer paso: definir las formulas
+- segundo paso: implementar formulas en Js
+- tercer paso : Crear funciones
+- Cuarto paso : integrar Js con html 
---
+++ fIgures.js
@@ -0,0 +1,22 @@
+const ladoCuadrado = 5;
+console.log("Laod del cuadrado miden " + ladoCuadrado + " Cm");
+
+const perimetroCuadrado = ladoCuadrado * 4; 
+console.log("el perimetro del cuadrado es " + perimetroCuadrado + "Cm");
+
+const areaCuadrado = ladoCuadrado * ladoCuadrado;
+console.log("EL area del cuadrado es " + areaCuadrado + " Cm")
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const baseTriangulo = 4;
+
+console.log(
+    "los lados de mi triangulo son : "+ 
+    ladoTriangulo1 
+    + "cm " 
+    + ladoTriangulo2 + 
+    "cm " 
+    + baseTriangulo 
+    + "cm "
+    );.000.toExponential.apply.0. 
