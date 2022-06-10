888853c99a3ef9de9f0b12de93a3bdb281ff017a
Raul Carbono <carbonoraul2@gmail.com>
2022-06-10 00:27:14 -0500

    pruebasJs

--- fIgures.js
+++ fIgures.js
@@ -17,6 +17,22 @@ function perimetroTriangulo (lado1, lado2, base){
 function areaTriangulo(base, altura ){
     return (base * altura) / 2;
 }
+function alturaTrianguloIsosceles(trianguloGrandeLadoA, trianguloGrandeLadoB, trianguloGrandeLadoBase) {
+    if (trianguloGrandeLadoA != trianguloGrandeLadoB) {
+        console.error("Los lados a y b no son iguales");
+    } else {
+        const trianguloPequenoLadoB = trianguloGrandeLadoBase / 2;
+        const trianguloPequenoLadoBase = trianguloGrandeLadoA;
+
+        const trianguloPequenoLadoBCuadrado = trianguloPequenoLadoB * trianguloPequenoLadoB;
+        const trianguloPequenoLadoBaseCuadrado = trianguloPequenoLadoBase * trianguloPequenoLadoBase;
+
+        const trianguloPequenoLadoA = Math.sqrt(trianguloPequenoLadoBaseCuadrado - trianguloPequenoLadoBCuadrado);
+
+        const trianguloGrandeAltura = trianguloPequenoLadoA;
+        return trianguloGrandeAltura;
+    }
+}
 
 console.groupEnd();
 
